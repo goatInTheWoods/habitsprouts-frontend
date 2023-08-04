b2cc48248af14e07d9b47cc120a2ddf934a1de54
Seohyoung <seohyoung.ha@gmail.com>
2023-08-03 17:50:43 -0700

    Add welcome card on habitList page

--- app/components/HabitList.js
+++ app/components/HabitList.js
@@ -7,6 +7,8 @@ import styled from 'styled-components';
 import StateContext from '../StateContext';
 import DispatchContext from '../DispatchContext';
 import { v4 as uuidv4 } from 'uuid';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const HabitList = () => {
   const appState = useContext(StateContext);
@@ -97,13 +99,44 @@ const HabitList = () => {
           closeModal={closeModal}
         />
       )}
-      <h1 className="text-primary">Habits</h1>
+      <h1 className="text-primary">
+        {appState?.user?.username
+          ? appState.user.username + `'s Habits`
+          : 'My Habits'}
+      </h1>
       <div className="d-flex flex-row-reverse mb-3 text-primary">
         <AddHabitButton onClick={() => openModal('add')}>
           <Plus />
         </AddHabitButton>
       </div>
       <div className="vstack gap-3">
+        {appState.habits.length === 0 && (
+          <Container>
+            <Card border="light" className="text-center">
+              <Card.Header
+                as="p"
+                className="bg-light fs-5 text-secondary"
+              >
+                Welcome to the Marathon!
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  I know making a habit is not easy. You are not
+                  alone. Start this race by creating a new habit card
+                  and tracking your achievements. You'll be amazed at
+                  how far you can go. Let's get started!
+                </Card.Text>
+                <Button
+                  className="text-white"
+                  variant="primary"
+                  onClick={() => openModal('add')}
+                >
+                  Create New Habit
+                </Button>
+              </Card.Body>
+            </Card>
+          </Container>
+        )}
         {appState.habits.map(habit => {
           return (
             <div
@@ -130,6 +163,12 @@ const HabitList = () => {
 
 const AddHabitButton = styled.button`
   all: unset;
+`;
+
+const Container = styled.div`
+  background: #fff;
+  box-shadow: 0px 6px 8px 0px rgba(0, 0, 0, 0.1);
+  font-size: 14px;
 `;
 
 export default HabitList;
