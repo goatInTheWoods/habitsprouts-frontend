0440916b823622dfcf098dff4e5cc160df3bbc6e
Seohyoung <seohyoung.ha@gmail.com>
2023-08-03 17:39:53 -0700

    Fix login button so it doesn't show in login page

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const HeaderLoggedOut = () => {
+  const location = useLocation();
   return (
-    <Link to="/login" className="col-md-auto">
-      <button className="btn btn-outline-secondary btn-sm rounded-0">
-        Sign In
-      </button>
-    </Link>
+    <>
+      {location.pathname !== '/login' && (
+        <Link to="/login" className="col-md-auto">
+          <button className="btn btn-outline-secondary btn-sm rounded-0">
+            Sign In
+          </button>
+        </Link>
+      )}
+    </>
   );
 };
 
