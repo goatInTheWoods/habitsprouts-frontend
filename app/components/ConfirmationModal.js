8398f4ffed3842bdcaf834273d3f84ed9352dbeb
Seohyoung <seohyoung.ha@gmail.com>
2023-12-13 16:40:19 -0800

    Fix to centralize Confirmation modal

--- app/components/ConfirmationModal.js
+++ app/components/ConfirmationModal.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useConfirmStatus, useActions } from '../store';
 
-function ConfirmationModal({
-  isOpen,
-  close,
-  title,
-  content,
-  submitBtnTxt,
-  handleSubmit,
-}) {
+function ConfirmationModal() {
+  const confirmStatus = useConfirmStatus();
+  const { closeConfirm } = useActions();
+
+  function handleClose() {
+    closeConfirm();
+  }
+
   return (
     <>
-      <Modal show={isOpen} onHide={close} centered>
+      <Modal show={confirmStatus.isOn} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>{title}</Modal.Title>
+          <Modal.Title>{confirmStatus.title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{content}</Modal.Body>
+        <Modal.Body
+          dangerouslySetInnerHTML={{ __html: confirmStatus.content }}
+        />
         <Modal.Footer>
-          <Button variant="secondary" onClick={close}>
+          <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="primary" onClick={handleSubmit}>
-            {submitBtnTxt}
+          <Button
+            variant="primary"
+            onClick={() => confirmStatus.submitFn()}
+          >
+            {confirmStatus.submitBtnText}
           </Button>
         </Modal.Footer>
       </Modal>
