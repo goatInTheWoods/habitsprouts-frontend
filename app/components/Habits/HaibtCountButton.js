b8fe2286f07d8f2661daaed46e1fd0059fb3322e
Seohyoung <seohyoung.ha@gmail.com>
2024-04-28 16:02:42 -0700

    Fix: change icon when completed

--- app/components/Habits/HaibtCountButton.js
+++ app/components/Habits/HaibtCountButton.js
@@ -5,7 +5,11 @@ import Smile from '../../images/smile.svg';
 
 const HabitCountButton = ({ isCompletedToday, onClick }) => {
   return (
-    <CountButtonContainer className="me-lg-3 " onClick={onClick}>
+    <CountButtonContainer
+      iscompletedtoday={isCompletedToday}
+      className="me-lg-3 "
+      onClick={onClick}
+    >
       {!isCompletedToday && <Check />}
       {isCompletedToday && <Smile />}
     </CountButtonContainer>
@@ -17,6 +21,9 @@ const CountButtonContainer = styled.button`
   height: 3.5rem;
   border-radius: 50%;
   background-color: #f0ff97;
+
+  background-color: ${props =>
+    props.iscompletedtoday ? '#f0ff97' : 'rgba(243, 255, 168, 0.35)'};
   border-style: none;
   filter: drop-shadow(2px 3px 9px rgba(122, 122, 122, 0.08));
 `;
