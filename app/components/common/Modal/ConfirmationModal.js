af0d8522bbaf5f263cd304c594fa1c24ac65912d
Seohyoung <seohyoung.ha@gmail.com>
2024-05-04 12:13:20 -0700

    Add: trigger submit when enter key down

--- app/components/common/Modal/ConfirmationModal.js
+++ app/components/common/Modal/ConfirmationModal.js
@@ -11,9 +11,20 @@ function ConfirmationModal() {
     closeConfirm();
   }
 
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      handleSubmit(event);
+    }
+  }
+
   return (
     <>
-      <Modal show={confirmStatus.isOn} onHide={handleClose} centered>
+      <Modal
+        show={confirmStatus.isOn}
+        onHide={handleClose}
+        onKeyPress={handleKeyPress}
+        centered
+      >
         <Modal.Header closeButton>
           <Modal.Title>{confirmStatus.title}</Modal.Title>
         </Modal.Header>
