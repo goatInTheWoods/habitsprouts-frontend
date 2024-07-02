c0f95e8a8dad7c66cb80414a4996ac19a6cd5b98
Seohyoung <seohyoung.ha@gmail.com>
2024-07-01 17:20:08 -0700

    feat: convert LogFilter component to TS

--- app/components/Logs/LogFilter.tsx
+++ app/components/Logs/LogFilter.tsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import { HabitDetail } from '@/types/log';
+
+interface LogFilterProps {
+  habitFilterList: HabitDetail[];
+  filter: string;
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+}
 
 function LogFilter({
-  habitList,
+  habitFilterList,
   filter,
-  setFilter
-}: $TSFixMe) {
+  setFilter,
+}: LogFilterProps) {
   const [title, setTitle] = useState('All');
 
-  function handleSelect(key: $TSFixMe, event: $TSFixMe) {
-    const selectedItemKey = event.target.getAttribute('data-key');
+  function handleSelect(
+    eventKey: string | null,
+    e: React.SyntheticEvent<unknown>
+  ) {
+    const selectedItemKey =
+      (e.target as HTMLElement).getAttribute('data-key') || '';
     setFilter(selectedItemKey);
   }
 
   useEffect(() => {
-    const currentHabit = habitList?.find(
-      (habit: $TSFixMe) => habit.id === filter
+    const currentHabit = habitFilterList?.find(
+      (habit: HabitDetail) => habit.id === filter
     );
     setTitle(currentHabit ? currentHabit.title : 'All');
   }, [filter]);
@@ -27,15 +37,13 @@ function LogFilter({
         id="dropdown-basic-button"
         variant={'secondary'}
         size="sm"
-        // @ts-expect-error TS(2322) FIXME: Type '{ children: string; id: string; variant: str... Remove this comment to see the full error message
-        drop="down"
       >
         {title}
       </Dropdown.Toggle>
       <Dropdown.Menu>
         <Dropdown.Item data-key={null}>All</Dropdown.Item>
-        {habitList &&
-          habitList.map((habit: $TSFixMe, i: $TSFixMe) => {
+        {habitFilterList &&
+          habitFilterList.map((habit: HabitDetail, i: number) => {
             return (
               <Dropdown.Item data-key={habit.id} key={i}>
                 {habit.title}
